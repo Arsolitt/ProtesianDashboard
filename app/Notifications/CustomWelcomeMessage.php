3850c4cb028eab2f20faf26f364312eddb238dce
Arsolitt <arsolitt@gmail.com>
2023-12-23 20:14:09 +0300

    Translate iteration 15

--- app/Notifications/CustomWelcomeMessage.php
+++ app/Notifications/CustomWelcomeMessage.php
@@ -7,7 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class WelcomeMessage extends Notification implements ShouldQueue
+class CustomWelcomeMessage extends Notification implements ShouldQueue
 {
     use Queueable;
 
